d86c6b9ca66fdeb06079e84028243f68924895f4
Ewa Kadziolka <kalistaewa@gmail.com>
2022-03-03 23:03:28 +0100

    add home page sceleton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
+import Navbar from '../components/navBar'
+
 const Home = () => {
+
+  const authToken = true
+
+  const hendleClick = () =>{
+    console.log('clicked')
+  }
+
+
     return (
-      <div>
-          <h1>C'est la page Home</h1>
+      <>
+      <Navbar></Navbar>
+      <div className="home">
+          <h1>Stackoverflou</h1>
+          <h2>bienvenue</h2>
+          <button className="primary_button" onClick={hendleClick}>
+            {authToken ? 'se deconecter' : 'se conecter'}
+
+          </button>
       </div>
+      </>
     );
   }
   export default Home;
